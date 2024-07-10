4fb8ad1282e8f005b3490dc8242977a56bd1858a
Arunsamuvel <arun15ee03@gmail.com>
2024-07-10 20:19:40 +0530

    created random function for generate multiple output

--- script/script.js
+++ script/script.js
@@ -1,5 +1,18 @@
 "use strict";
-function check(weapon) {
-  console.log(weapon);
+
+const userChoiceEl = document.getElementById("user-Choice");
+const compChoiceEl = document.getElementById("comp-choice");
+// helper variables
+const weapons = ["rock", "paper", "scissor"];
+
+function generateRandomNumber(n) {
+  return Math.trunc(Math.random() * n);
 }
-// const userChoiceEl = document.getElementById("user_Choice");
+function check(weapon) {
+  const userChoice = weapon;
+  const compChoice = weapons[generateRandomNumber(weapons.length)];
+
+  userChoiceEl.innerText = "User Choose" + " " + userChoice;
+  compChoiceEl.innerText = "comp Choose" + " " + compChoice;
+  compareChoice(userChoice, compChoice);
+}
